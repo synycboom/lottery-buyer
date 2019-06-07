b515db81ad7a2c0dc155e2d0747a79bdb25419d1
rittichai.h <rittichai.h@linecorp.com>
2019-06-07 15:52:00 +0700

    add search

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
+  <div class="container">
   <div id="app">
     <router-view/>
   </div>
+  </div>
 </template>
-
 <style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,10 +1,39 @@
 <template>
-  <div> Search </div>
+  <div><h1>ค้นหา </h1>
+    <div class="row">
+      <div class="col-10">
+        <b-form-input v-model="text" placeholder="ใส่เลขที่คุณต้องการค้นหา"></b-form-input>
+      </div>
+      <div class="col">
+        <b-button @click="displaySearch()">Search</b-button>
+      </div>
+    </div>
+    <br/>
+    <div class="row">
+      <div class="col">
+        <b-table striped hover :items="items"></b-table>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-
-export default {
-  name: 'Search',
-};
+  export default {
+    data() {
+      return {
+        text: '',
+        items: []
+      };
+    },
+    methods: {
+      displaySearch() {
+        this.items = [
+          {match_number:110110},
+          {match_number:110111},
+          {match_number:110112},
+          {match_number:110113},
+        ];
+      }
+    }
+  };
 </script>

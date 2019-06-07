5c3ba47d7789eb2227f013f0519c2de530900d3e
rittichai.h <rittichai.h@linecorp.com>
2019-06-07 16:19:38 +0700

    update

--- src/views/Recommend.vue
+++ src/views/Recommend.vue
@@ -1,5 +1,33 @@
 <template>
-  <div> Recommend </div>
+  <div>
+    <div><h1>เลขเด็ด</h1></div>
+    <div class="row">
+      <div class="col">
+        <div class="card" style="width: 18rem;">
+          <img src="https://www.thairath.co.th/media/T18CT60LAESA6YvcWyjDtz99vNyMKUZA6A361Q9lf2tm4J5QTb68yto.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <p class="card-text">ฮือฮา แก้บนไข่ไก่ 2 หมื่นฟอง หลังถูกสลากกว่า 40...</p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card" style="width: 18rem;">
+          <img src="https://www.thairath.co.th/media/T18CT60LAESA6YvcWyjDtz98rTMNxctfC9uFhNbETDT5x7b8vDS5Hsa.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <p class="card-text">แห่ส่องเลขเด็ดขันน้ำมนต์ ปู่ศรีสุทโธแม่ย่าศรีปทุมมา</p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card" style="width: 18rem;">
+          <img src="https://www.thairath.co.th/media/T18CT60LAESA6YvcWyjDs8lO8llNGLBr3wBs3M1M5giAJApcvnVz9d6.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <p class="card-text">× ดวงเฮง! ตลกชื่อดังถูกเลขท้าย 2 ตัว 60 ใบ</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
